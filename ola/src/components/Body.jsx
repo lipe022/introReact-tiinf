13ab51527feb8b9d1de13982de685c2dc81eb60e
lipe022 <luizfeliperibeiro256@gmail.com>
2025-05-26 22:18:46 -0300

    Criação site Header

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='Body'>
+            <h2 className='titulo2'>Dinos são muito legais</h2><br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, facilis. Perspiciatis optio eligendi consequuntur porro nesciunt vitae, laboriosam debitis sed provident <br />
+            ipsum voluptas distinctio corporis ipsam, 
+            magnam eos quibusdam nihil.</p>
+        </div>
+    )
+}
+
+export default Body
---
+++ ola/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='Header'>
+        <center>
+        <h1>Dinossauros 🦖☄️</h1>
+        </center>
+    </div>
+  )
+}
+
+export default Header
